6f38c35fddd2958372a396707c9baa4de7a0684f
Jason Brady <jasonmbrady42@gmail.com>
2020-07-20 17:13:32 -0700

    Finished Products assignment 2

--- client/src/components/AllProducts.jsx
+++ client/src/components/AllProducts.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
-const AllProducts = (props) => {
+const AllProducts = () => {
+    const [products, setProducts] = useState([{
+        title: "",
+        price: 0,
+        desc: ""
+    }]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/products")
+            .then(res => setProducts(res.data.products))
+            .catch(err => console.log(err));
+    }, []);
 
     return (
-        <div></div>
+        <div>
+            {
+                products.map((product, idx) => {
+                    return (
+                        <div key={idx} style={{ display: "inline-block", margin: "25px" }}>
+                            <h3>{product.title}</h3>
+                            <p>Price: ${product.price / 100}</p>
+                            <p>Description: {product.desc}</p>
+                        </div>
+                    );
+                })
+            }
+        </div>
     )
 }
 export default AllProducts;
